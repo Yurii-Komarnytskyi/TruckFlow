cdd2119704908f07171f65df16e6e0ee2dd0d49a
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-03-16 16:03:15 +0100

    renamed TrackAndTraceApplication -> TransportationManagementApplication

---
+++ src/main/java/com/ykomarnytskyi2022/TransportationManagementApplication.java
@@ -0,0 +1,11 @@
+package com.ykomarnytskyi2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportationManagementApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TransportationManagementApplication.class, args);
+	}
+}
