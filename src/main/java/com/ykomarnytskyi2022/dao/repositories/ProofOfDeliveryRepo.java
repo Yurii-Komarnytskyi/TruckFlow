d860407e049bfcf88b1fd253b86813ab104bf011
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-03-22 21:58:07 +0100

    created JpaRepositories for entities: ProofOfDelivery, BillOfLading and Driver

---
+++ src/main/java/com/ykomarnytskyi2022/dao/repositories/ProofOfDeliveryRepo.java
@@ -0,0 +1,9 @@
+package com.ykomarnytskyi2022.dao.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ykomarnytskyi2022.dao.entity.ProofOfDelivery;
+
+public interface ProofOfDeliveryRepo extends JpaRepository<ProofOfDelivery, Long> {
+
+}
---
+++ src/main/java/com/ykomarnytskyi2022/repositories/BillOfLadingRepo.java
@@ -0,0 +1,9 @@
+package com.ykomarnytskyi2022.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ykomarnytskyi2022.dao.entity.BillOfLading;
+
+public interface BillOfLadingRepo extends JpaRepository<BillOfLading, Long> {
+
+}
