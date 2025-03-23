3d6ea58f207005c73ebbf8f2b6d9bc04d4bf4804
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-03-23 18:46:10 +0100

    created JPA repo for driver entity

---
+++ src/main/java/com/ykomarnytskyi2022/dao/repositories/DriverRepository.java
@@ -0,0 +1,11 @@
+package com.ykomarnytskyi2022.dao.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ykomarnytskyi2022.dao.entity.Driver;
+
+public interface DriverRepository extends JpaRepository<Driver, Long> {
+	Optional<Driver> findDriverByCellPhone(String cellPhone);
+}
