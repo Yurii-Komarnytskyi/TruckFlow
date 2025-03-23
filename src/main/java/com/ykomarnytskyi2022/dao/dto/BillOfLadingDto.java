6ddf7ed2f4cbb58f0d246f241c7fd6dc862fc054
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-03-23 18:46:45 +0100

    A bunch of dummy DTO types for entities: BillOfLading, Driver, DriverShipment, LogisticsCoordinator, Pageable, ProofOfDelivery, RoadAccident, TransportationIssue

---
+++ src/main/java/com/ykomarnytskyi2022/dao/dto/BillOfLadingDto.java
@@ -0,0 +1,10 @@
+package com.ykomarnytskyi2022.dao.dto;
+
+import com.ykomarnytskyi2022.dao.entity.PDFDocument;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class BillOfLadingDto  extends PDFDocument{
+
+}
---
+++ src/main/java/com/ykomarnytskyi2022/dao/dto/ShipmentDto.java
@@ -0,0 +1,13 @@
+package com.ykomarnytskyi2022.dao.dto;
+
+import com.ykomarnytskyi2022.freight.ShipmentStatus;
+
+public class ShipmentDto {
+
+	private ShipmentStatus status;
+	
+	public void setStatus(ShipmentStatus status) {
+		this.status = status;
+	}
+
+}
