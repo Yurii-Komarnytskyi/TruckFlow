47d97e5d5027b58f96983eb5d166d16acb972fe6
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-04-05 20:17:28 +0200

    Fix: BillOfLadingDto, removing accidentally added Entity annot. and extends keyword

--- src/main/java/com/ykomarnytskyi2022/dao/dto/BillOfLadingDto.java
+++ src/main/java/com/ykomarnytskyi2022/dao/dto/BillOfLadingDto.java
@@ -1,10 +1,5 @@
 package com.ykomarnytskyi2022.dao.dto;
 
-import com.ykomarnytskyi2022.dao.entity.PDFDocument;
-
-import jakarta.persistence.Entity;
-
-@Entity
-public class BillOfLadingDto  extends PDFDocument{
+public class BillOfLadingDto {
 
 }
