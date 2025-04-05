30326d009d229400318b5fd345465a5043cbc88e
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-04-05 23:31:39 +0200

    Mock class ShipmentSearchCriteria

---
+++ src/main/java/com/ykomarnytskyi2022/service/ShipmentSearchCriteria.java
@@ -0,0 +1,16 @@
+package com.ykomarnytskyi2022.service;
+
+import java.time.LocalDate;
+
+public class ShipmentSearchCriteria {
+//	private String status;          // Filter by shipment status (e.g., "IN_TRANSIT")
+//    private Long driverId;          // Filter by assigned driver ID
+//    private Long coordinatorId;     // Filter by assigned coordinator ID
+//    private String origin;          // Filter by origin location (e.g., city name)
+//    private String destination;     // Filter by destination location
+//    private LocalDate createdAfter; // Filter shipments created after this date
+//    private LocalDate createdBefore;// Filter shipments created before this date
+//    private LocalDate deliveredAfter;  // Filter shipments delivered after this date
+//    private LocalDate deliveredBefore; // Filter shipments delivered before this date
+//    private String customerName;    // Filter by customer name
+}
