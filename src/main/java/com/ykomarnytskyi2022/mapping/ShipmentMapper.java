deb702d60d3d596ff75742aaa3023aa6f2dc8844
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-04-06 12:14:56 +0200

    added mock mapper class ShipmentMapper

---
+++ src/main/java/com/ykomarnytskyi2022/mapping/ShipmentMapper.java
@@ -0,0 +1,17 @@
+package com.ykomarnytskyi2022.mapping;
+
+import org.modelmapper.AbstractConverter;
+import org.springframework.stereotype.Component;
+
+import com.ykomarnytskyi2022.dao.dto.ShipmentDto;
+import com.ykomarnytskyi2022.dao.entity.Shipment;
+
+@Component
+public class ShipmentMapper extends AbstractConverter<Shipment, ShipmentDto> {
+
+	@Override
+	protected ShipmentDto convert(Shipment source) {
+		return null;
+	}
+
+}
