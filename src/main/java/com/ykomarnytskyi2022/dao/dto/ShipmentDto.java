737df4dd917080765b6a4107e9eeed4c476d23e1
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-04-07 21:20:07 +0200

    added field LogisticsCoordinator in dto class ShipmentDto

--- src/main/java/com/ykomarnytskyi2022/dao/dto/ShipmentDto.java
+++ src/main/java/com/ykomarnytskyi2022/dao/dto/ShipmentDto.java
@@ -3,11 +3,33 @@ package com.ykomarnytskyi2022.dao.dto;
 import com.ykomarnytskyi2022.enums.ShipmentStatus;
 
 public class ShipmentDto {
-
 	private ShipmentStatus status;
+
+	private DriverDto driver;
 	
+	private LogisticsCoordinatorDto coordinator;
+
+	public ShipmentStatus getStatus() {
+		return status;
+	}
+
 	public void setStatus(ShipmentStatus status) {
 		this.status = status;
 	}
 
+	public DriverDto getDriver() {
+		return driver;
+	}
+
+	public void setDriver(DriverDto driver) {
+		this.driver = driver;
+	}
+
+	public LogisticsCoordinatorDto getCoordinator() {
+		return coordinator;
+	}
+
+	public void setCoordinator(LogisticsCoordinatorDto coordinator) {
+		this.coordinator = coordinator;
+	}
 }
