edfa316d3cca125e16be7df66b454ff7987657cb
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-04-07 23:22:28 +0200

    Added mappings in entity Shipment : - Driver entity - LogisticsCoordinator entity - Customer entity
    - added getters and setters for said mapping fields

--- src/main/java/com/ykomarnytskyi2022/dao/entity/Shipment.java
+++ src/main/java/com/ykomarnytskyi2022/dao/entity/Shipment.java
@@ -2,12 +2,19 @@ package com.ykomarnytskyi2022.dao.entity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+import com.ykomarnytskyi2022.enums.ShipmentStatus;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 
@@ -17,8 +24,14 @@ public class Shipment {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@OneToOne(fetch = FetchType.LAZY)
+	@ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST})
 	private Driver driver;
+	
+	@ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST})
+	private LogisticsCoordinator coordinator;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Customer customer;
 	
 	@OneToOne(fetch = FetchType.LAZY)
 	private BillOfLading billOfLading;
@@ -28,6 +41,9 @@ public class Shipment {
 	
 	@OneToMany(mappedBy = "shipment", fetch = FetchType.LAZY)
 	private List<RoadAccident> roadAccidents = new ArrayList<>();
+	
+	@Enumerated(EnumType.STRING)
+	private ShipmentStatus status;
 	
 	public Shipment() {
 		// default no-argument constructor for Spring JPA	
@@ -63,5 +79,41 @@ public class Shipment {
 
 	public void setProofOfDelivery(ProofOfDelivery proofOfDelivery) {
 		this.proofOfDelivery = proofOfDelivery;
+	}
+
+	public ShipmentStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(ShipmentStatus status) {
+		this.status = status;
+	}
+
+	public LogisticsCoordinator getCoordinator() {
+		return coordinator;
+	}
+
+	public void setCoordinator(LogisticsCoordinator coordinator) {
+		this.coordinator = coordinator;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public List<RoadAccident> getRoadAccidents() {
+		return roadAccidents;
+	}
+
+	public void setRoadAccidents(List<RoadAccident> roadAccidents) {
+		this.roadAccidents = roadAccidents;
+	}
+	
+	public boolean hasDriverAssigned() {
+		return Objects.nonNull(driver) && Objects.nonNull(driver.getId());
 	}
 }
