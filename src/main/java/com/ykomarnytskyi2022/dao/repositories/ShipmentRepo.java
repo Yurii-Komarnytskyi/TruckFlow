2accca4aa0c1208913976d089a3c954a0ee7e7de
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-04-07 23:20:35 +0200

    Added jpa method queries in ShipmentRepo : - findAllByDriverId - findAllByStatus - findAllByCoordinatorId - findAllByCustomerId

--- src/main/java/com/ykomarnytskyi2022/dao/repositories/ShipmentRepo.java
+++ src/main/java/com/ykomarnytskyi2022/dao/repositories/ShipmentRepo.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ykomarnytskyi2022.dao.entity.Shipment;
+import com.ykomarnytskyi2022.enums.ShipmentStatus;
 
 public interface ShipmentRepo extends JpaRepository<Shipment, Long> {
-	List<Shipment> findAllByDriverId(Long driverId);
+	List<Shipment> findAllByDriverId(Long id);
+	List<Shipment> findAllByStatus(ShipmentStatus status);
+	List<Shipment> findAllByCoordinatorId(Long id);
+	List<Shipment> findAllByCustomerId(Long id);
 }
