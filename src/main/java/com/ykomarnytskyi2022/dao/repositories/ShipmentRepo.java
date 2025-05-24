35ab4f159fc5025e8d31d041c5c6e8153c400adc
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-05-24 14:51:31 +0200

    ShipmentRepo interface extends JpaSpecificationExecutor

--- src/main/java/com/ykomarnytskyi2022/dao/repositories/ShipmentRepo.java
+++ src/main/java/com/ykomarnytskyi2022/dao/repositories/ShipmentRepo.java
@@ -3,11 +3,12 @@ package com.ykomarnytskyi2022.dao.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.ykomarnytskyi2022.dao.entity.Shipment;
 import com.ykomarnytskyi2022.enums.ShipmentStatus;
 
-public interface ShipmentRepo extends JpaRepository<Shipment, Long> {
+public interface ShipmentRepo extends JpaRepository<Shipment, Long>, JpaSpecificationExecutor<Shipment> {
 	List<Shipment> findAllByDriverId(Long id);
 	List<Shipment> findAllByStatus(ShipmentStatus status);
 	List<Shipment> findAllByCoordinatorId(Long id);
