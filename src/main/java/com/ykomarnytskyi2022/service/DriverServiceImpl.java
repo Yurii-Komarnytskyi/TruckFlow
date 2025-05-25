d500ae19e6a692e05b58c2890ac07547d6be498e
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-05-25 18:32:24 +0200

    refactored DriverServiceImpl removed unneccessary throws declarations removed unneccessaty try catrch blocks using @NotNull for method arguments

--- src/main/java/com/ykomarnytskyi2022/service/DriverServiceImpl.java
+++ src/main/java/com/ykomarnytskyi2022/service/DriverServiceImpl.java
@@ -1,7 +1,7 @@
 package com.ykomarnytskyi2022.service;
 
 import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 import com.ykomarnytskyi2022.dao.dto.DriverDto;
 import com.ykomarnytskyi2022.dao.dto.DriverShipmentDto;
@@ -20,12 +21,15 @@ import com.ykomarnytskyi2022.enums.ShipmentStatus;
 
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 @Service
+@Validated
 public class DriverServiceImpl implements DriverService {
 	private final DriverRepo driverRepo;
 	private final ShipmentManagementService shipmentManagementService;
 	private final ModelMapper mapper;
+	
 	private static final Logger LOGGER = LoggerFactory.getLogger(DriverServiceImpl.class);
 	private static final int STANDARD_PAGE_SIZE = 12;
 
@@ -39,43 +43,47 @@ public class DriverServiceImpl implements DriverService {
 	}
 
 	@Override
-	public void acceptShipment(@Valid DriverShipmentDto driverShipment) throws EntityNotFoundException , UnsupportedOperationException {
-		try {
-			DriverDto driver = mapper.map(driverRepo.findById(driverShipment.getDriverId()).orElseThrow(), DriverDto.class);
-			shipmentManagementService.assignDriverForShipment(driver, driverShipment.getId());
-		} catch (NoSuchElementException e) {
-			throw new EntityNotFoundException("Driver entity with Id : %s does not exist".formatted(driverShipment.getDriverId()));
-		} catch (EntityNotFoundException | UnsupportedOperationException e) {
-			throw e;
+	public void acceptShipment(@Valid DriverShipmentDto driverShipment) {
+		if(Objects.isNull(driverShipment)) {
+			throw new IllegalArgumentException("driverShipment argument cannot be null");
 		}
-				
+		shipmentManagementService.assignDriverForShipment(driverShipment, driverShipment.getId());
 	}
 
 	@Override
-	public void updateProgressOnShipment(Long shipmentId, ShipmentStatus status) {
-		Optional<ShipmentDto> shipmentOptional = shipmentManagementService.findShipmentById(shipmentId);
-		if (shipmentOptional.isPresent()) {
-			ShipmentDto shipment = shipmentOptional.get();
+	public void updateProgressOnShipment(@NotNull Long shipmentId, ShipmentStatus status) {
+		if(Objects.isNull(status)) {
+			throw new IllegalArgumentException("status arg cannot be null");
+		} else {
+			ShipmentDto shipment = shipmentManagementService.findShipmentById(shipmentId)
+					.orElseThrow(() -> new EntityNotFoundException("Shipment entity with Id: %s does not exist".formatted(shipmentId)));
 			shipment.setStatus(status);
 			shipmentManagementService.updateShipment(shipment);
+			
 		}
 	}
 
 	@Override
-	public PageableDto<DriverShipmentDto> getDriverShipments(Long driverId) {
+	public PageableDto<DriverShipmentDto> getDriverShipments(@NotNull Long driverId) {
 		List<DriverShipmentDto> page = shipmentManagementService.getShipmentsAssignedToDriver(driverId).page().stream()
 				.map(s -> mapper.map(s, DriverShipmentDto.class)).toList();
 		return new PageableDto<>(page, page.size(), PageableDto.FIRST_PAGE_INDEX, PageableDto.calculateTotalPages(page.size(), STANDARD_PAGE_SIZE));
 	}
 
 	@Override
-	public Optional<DriverDto> findDriverById(Long driverId) {
-		return Optional.of(mapDriverToDto(driverRepo.getReferenceById(driverId)));
+	public Optional<DriverDto> findDriverById(@NotNull Long driverId) {
+		Optional<Driver> driverOpt = driverRepo.findById(driverId);
+		return (driverOpt.isPresent())
+				? Optional.of(mapDriverToDto(driverOpt.get())) 
+				: Optional.empty();
 	}
 
 	@Override
-	public Optional<DriverDto> findDriverByCellPhone(String cellPhone) {
-		return Optional.ofNullable(mapDriverToDto(driverRepo.findDriverByCellPhone(cellPhone).orElseThrow()));
+	public Optional<DriverDto> findDriverByCellPhone(@NotNull String cellPhone) {
+		Optional<Driver> driverOpt = driverRepo.findDriverByCellPhone(cellPhone);
+		return (driverOpt.isPresent()) 
+				? Optional.of(mapDriverToDto(driverOpt.get())) 
+				: Optional.empty();
 	}
 
 	private final DriverDto mapDriverToDto(Driver driver) {
