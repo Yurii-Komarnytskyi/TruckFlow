7cc92fd21dff96149b2772671633bbf1dd92b6b1
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-05-25 18:24:55 +0200

    organized imports in DriverService

--- src/main/java/com/ykomarnytskyi2022/service/DriverService.java
+++ src/main/java/com/ykomarnytskyi2022/service/DriverService.java
@@ -1,15 +1,10 @@
 package com.ykomarnytskyi2022.service;
 
-import java.util.List;
 import java.util.Optional;
 
-import com.ykomarnytskyi2022.dao.dto.BillOfLadingDto;
 import com.ykomarnytskyi2022.dao.dto.DriverDto;
 import com.ykomarnytskyi2022.dao.dto.DriverShipmentDto;
 import com.ykomarnytskyi2022.dao.dto.PageableDto;
-import com.ykomarnytskyi2022.dao.dto.ProofOfDeliveryDto;
-import com.ykomarnytskyi2022.dao.dto.RoadAccidentDto;
-import com.ykomarnytskyi2022.dao.dto.TransportationIssueDto;
 import com.ykomarnytskyi2022.enums.ShipmentStatus;
 
 public interface DriverService {
