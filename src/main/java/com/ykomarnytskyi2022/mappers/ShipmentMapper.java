b4565e265346622e6ebc5ed7b503823ae1959cdd
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-06-11 20:37:39 +0200

    implemented ShipmentMapper, intoduced AbstractConverterSourceIsNullException

--- src/main/java/com/ykomarnytskyi2022/mappers/ShipmentMapper.java
+++ src/main/java/com/ykomarnytskyi2022/mappers/ShipmentMapper.java
@@ -1,17 +1,32 @@
 package com.ykomarnytskyi2022.mappers;
 
+import java.util.Objects;
+
 import org.modelmapper.AbstractConverter;
 import org.springframework.stereotype.Component;
 
 import com.ykomarnytskyi2022.dao.dto.ShipmentDto;
 import com.ykomarnytskyi2022.dao.entity.Shipment;
+import com.ykomarnytskyi2022.exceptions.AbstractConverterSourceIsNullException;
 
 @Component
 public class ShipmentMapper extends AbstractConverter<Shipment, ShipmentDto> {
 
 	@Override
 	protected ShipmentDto convert(Shipment source) {
-		return null;
+		if (Objects.isNull(source)) {
+			throw new AbstractConverterSourceIsNullException();
+		}
+		ShipmentDto destination = new ShipmentDto();
+		destination.setId(source.getId());
+		destination.setDeliveredAt(source.getDeliveredAt());
+		destination.setDestination(source.getDestination());
+		destination.setGoodsDescription(source.getGoodsDescription());
+		destination.setOrigin(source.getOrigin());
+		destination.setPickedUpAt(source.getPickedUpAt());
+		destination.setStatus(source.getStatus());
+
+		return destination;
 	}
 
 }
